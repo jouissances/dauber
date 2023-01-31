f5b7699b172a0f09ee573f95950ca81e8c4237bc
Janice Darikho <janicedarikho@gmail.com>
2023-01-31 12:32:45 +0800

    migrate to postgres

--- db/migrate/20230115044910_create_daubs.rb
+++ db/migrate/20230115044910_create_daubs.rb
@@ -2,13 +2,14 @@ class CreateDaubs < ActiveRecord::Migration[7.0]
   def change
     create_table :daubs do |t|
       t.string :category
-      t.string :genre
+      t.string :genre, array: true, default: []
       t.string :title
       t.text :text_url
       t.string :author
       t.date :publication_date
       t.string :published_by
       t.text :image_url
+      t.text :body
 
       t.timestamps
     end
